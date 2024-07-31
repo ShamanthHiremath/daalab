7b0aa306f4debda9ee8ef6ba9c86f6cf0ee87b38
Shamanth M Hiremath <123861244+ShamanthHiremath@users.noreply.github.com>
2024-08-01 00:54:02 +0530

    Update bellman_ford.py

--- LabExam/bellman_ford.py
+++ LabExam/bellman_ford.py
@@ -1,4 +1,4 @@
-def bellmanFord(n, m, src, edges):
+def bellmanFord(n, src, edges):
     # Initialize distances to infinity
     dist = [float('inf')] * n
     dist[src] = 0  # Distance to source vertex is 0
@@ -17,28 +17,46 @@ def bellmanFord(n, m, src, edges):
 
     return dist
 
+def inputEdges():
+    edges = []
+    e = int(input("Enter the no. of edges: "))
+    print("Enter edge pair as u -> v")
+    for i in range(e):
+        u = int(input(f"Pair {i}: "))
+        v = int(input("->"))
+        dir = int(input("Directed?: "))
+        wt = int(input("Enter weight: "))
+        
+        edges.append([u, v, wt])
+
+        if (not dir):
+            edges.append([v, u, wt])
+        
+    return edges
+
 
 def main():
-    n = 5  # Number of vertices
-    m = 7  # Number of edges
+    vertices = int(input("\nEnter number of vertices: "))  
+    
+    # # Example edges list
+    # edges = [
+    #     [0, 1, -1],  # Edge from vertex 0 to vertex 1 with weight -1
+    #     [0, 2, 4],   # Edge from vertex 0 to vertex 2 with weight 4
+    #     [1, 2, 3],   # Edge from vertex 1 to vertex 2 with weight 3
+    #     [1, 3, 2],   # Edge from vertex 1 to vertex 3 with weight 2
+    #     [1, 4, 2],   # Edge from vertex 1 to vertex 4 with weight 2
+    #     [3, 2, 5],   # Edge from vertex 3 to vertex 2 with weight 5
+    #     [3, 1, 1]    # Edge from vertex 3 to vertex 1 with weight 1
+    # ]
+    
+    edges = inputEdges()
 
-    # Example edges list
-    edges = [
-        [0, 1, -1],  # Edge from vertex 0 to vertex 1 with weight -1
-        [0, 2, 4],   # Edge from vertex 0 to vertex 2 with weight 4
-        [1, 2, 3],   # Edge from vertex 1 to vertex 2 with weight 3
-        [1, 3, 2],   # Edge from vertex 1 to vertex 3 with weight 2
-        [1, 4, 2],   # Edge from vertex 1 to vertex 4 with weight 2
-        [3, 2, 5],   # Edge from vertex 3 to vertex 2 with weight 5
-        [3, 1, 1]    # Edge from vertex 3 to vertex 1 with weight 1
-    ]
+    src = int(input("\nEnter source vertex: "))
 
-    src = 0  # Source vertex for Bellman-Ford algorithm
-
-    shortest_distances = bellmanFord(n, m, src, edges)
+    shortest_distances = bellmanFord(vertices, src, edges)
 
     # Output the shortest distances from the source vertex
-    for i in range(n):
+    for i in range(vertices):
         print(f"Distance to vertex {i} from source: ", end="")
         if shortest_distances[i] == float('inf'):
             print("INF")
@@ -48,3 +66,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
